e0db88c72dad7624df5e79478c41c83adb6bb22f
S M AL RABBI <rabbithedeveloper@gmail.com>
2026-02-09 17:04:07 +0600

    Update page.tsx

--- src/app/(theme-3-old)/th_3/page.tsx
+++ src/app/(theme-3-old)/th_3/page.tsx
@@ -5,7 +5,7 @@ import AllProduct from "./_components/all-product"
 import Scroll from "./_components/Scroll"
 import { getDomainHeaders } from "@/lib/domain"
 import { api } from "@/lib/api-client"
-import { IProductsApiResponse} from "./types/product"
+import { IProductsApiResponse } from "./types/product"
 
 
 
@@ -28,27 +28,27 @@ export default async function Home({
   const totalPages = response.last_page
 
   return (
-  <div className="relative min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 overflow-x-hidden transition-colors duration-500">
+    <div className="relative min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 overflow-x-hidden transition-colors duration-500">
 
-  {/* Decorative blobs */}
-  <div
-    className="blob blob-1 fixed top-[-10%] left-[-10%] w-[500px] h-[500px] rounded-full opacity-60 blur-[80px] -z-10
+      {/* Decorative blobs */}
+      <div
+        className="blob blob-1 fixed top-[-10%] left-[-10%] w-[500px] h-[500px] rounded-full opacity-60 blur-[80px] -z-10
                bg-[radial-gradient(circle,rgb(224,231,255)_0%,rgba(255,255,255,0)_70%)]
                dark:bg-[radial-gradient(circle,rgb(55,65,81)_0%,rgba(0,0,0,0)_70%)]"
-  />
-  <div
-    className="blob blob-2 fixed bottom-0 right-[-10%] w-[600px] h-[600px] rounded-full opacity-60 blur-[80px] -z-10
+      />
+      <div
+        className="blob blob-2 fixed bottom-0 right-[-10%] w-[600px] h-[600px] rounded-full opacity-60 blur-[80px] -z-10
                bg-[radial-gradient(circle,rgb(255,228,230)_0%,rgba(255,255,255,0)_70%)]
                dark:bg-[radial-gradient(circle,rgb(139,92,246)_0%,rgba(0,0,0,0)_70%)]"
-  />
+      />
 
-  {/* Sections */}
-  <Banner />
-  <Category />
-  <MiddleBanner />
-  <AllProduct products={products} totalPages={totalPages} />
-  <Scroll />
-</div>
+      {/* Sections */}
+      <Banner />
+      <Category />
+      <MiddleBanner />
+      <AllProduct products={products} totalPages={totalPages} />
+      <Scroll />
+    </div>
 
   )
 }
